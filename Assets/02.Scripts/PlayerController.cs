373db26d09e077e13d8b2370a48c6d84e5995a9f
DESKTOP-FVBU6NK\Sejin <sjb0710@gmail.com>
2025-05-29 10:41:43 +0900

    Fix: PlayerController부분의 오브젝트 상호작용 코드 추가
    이전 커밋 누락 부분 추가한 것
    
    Resolves: 직전 커밋

--- Assets/02.Scripts/PlayerController.cs
+++ Assets/02.Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public interface IInteractable
 {
-    InteractableController GetInteractableInfo();
+    ObjectData GetInteractableInfo();
 }
 
 public class PlayerController : MonoBehaviour

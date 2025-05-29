0359b139af35a9e391f9804588ed8652ad667aa0
kimheejoo0828 <o1043381828@nate.com>
2025-05-29 20:01:15 +0900

    비콘효과음

---
+++ Assets/02.Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+    public string sfxName = "boxHit";
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Beacon") || collision.contacts.Length > 0)
+        {
+            SoundManager.Instance.PlaySFX(sfxName);
+        }
+    }
+}

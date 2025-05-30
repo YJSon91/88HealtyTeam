3a964493db192ef0cbe18089bdfa97b7b2198197
DESKTOP-FVBU6NK\Sejin <sjb0710@gmail.com>
2025-05-30 20:33:22 +0900

    Fix: Tutorial Coroutine이 올바르게 동작하지 않는 문제 수정

--- Assets/02.Scripts/UI/TutorialUI.cs
+++ Assets/02.Scripts/UI/TutorialUI.cs
@@ -8,6 +8,8 @@ public class TutorialUI : MonoBehaviour
 {
     public TextMeshProUGUI tutorialText;
     public GameObject panel;
+    public Coroutine tutorialCoroutine;
+    private List<TutorialType> alreadyShownTutorials = new List<TutorialType>(); // 이미 표시한 튜토리얼 리스트
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +21,31 @@ public class TutorialUI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ShowTutorial(TutorialType type, string text, float duration)
+    {
+        if (alreadyShownTutorials.Contains(type))
+            return;
+
+        alreadyShownTutorials.Add(type);
+
+        if (tutorialCoroutine != null)
+        {
+            StopCoroutine(tutorialCoroutine);
+        }
+
+        tutorialText.text = text;
+        gameObject.SetActive(true);
+
+        tutorialCoroutine = StartCoroutine(DisableTutorialUI(duration));
+    }
+
+    private IEnumerator DisableTutorialUI(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        gameObject.SetActive(false);
+        tutorialCoroutine = null;
     }
 }
